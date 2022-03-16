f0b0deb6fe37b926cf19e1833a5901b7995238ab
Mojx Mojx <omarmunozcerda@gmail.com>
2022-03-16 15:17:30 -0500

    added routing, sign in component, and firebase kubrarues abd utils to save users

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import { initializeApp } from 'firebase/app';
+import {
+	getAuth,
+	signInWithPopup,
+	signInWithRedirect,
+	GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCg9bFOaVroV68nxUe1-tdTRLBe7X3UbkQ',
+	authDomain: 'crwn-clothing-db-46b8d.firebaseapp.com',
+	projectId: 'crwn-clothing-db-46b8d',
+	storageBucket: 'crwn-clothing-db-46b8d.appspot.com',
+	messagingSenderId: '1344830859',
+	appId: '1:1344830859:web:9d5dfdfc8cbcd3a1c2438e',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+	prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, 'users', userAuth.uid);
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists());
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log('error creating the user', error.message);
+		}
+	}
+
+	return userDocRef;
+};
